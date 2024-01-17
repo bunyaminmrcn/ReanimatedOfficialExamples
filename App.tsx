59988046aef94fda9ec1dc8a98f0fc90a23516be
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-17 12:51:59 +0300

    Lesson02 complate

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import Lesson01 from './src/lesson_01';
+import Lesson02_01 from './src/lesson_02-01';
+import Lesson02_02 from './src/lesson_02-02';
 
 
 export default function App()
 {
-  return <Lesson01 />
+  return <Lesson02_02 />
 }
